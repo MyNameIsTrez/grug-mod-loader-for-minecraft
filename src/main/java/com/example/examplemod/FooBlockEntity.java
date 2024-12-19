164f356e77d62d4bbfc83133215e7414a7af34ab
MyNameIsTrez <welfje@gmail.com>
2024-12-19 21:22:57 +0100

    Spawn a diamond every 0.5 seconds

--- src/main/java/com/example/examplemod/FooBlockEntity.java
+++ src/main/java/com/example/examplemod/FooBlockEntity.java
@@ -1,15 +1,30 @@
 package com.example.examplemod;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 
 public class FooBlockEntity extends BlockEntity {
+    private int ticks;
+
     public FooBlockEntity(BlockPos pos, BlockState state) {
         super(ExampleMod.FOO_BLOCK_ENTITY.get(), pos, state);
     }
 
     public void tick() {
-        System.out.println("Hello from \"tick\"!");
+        if (++this.ticks % 10 == 0) {
+            this.ticks = 0;
+
+            System.out.println("Spawning diamond");
+
+            Vec3 spawnPos = worldPosition.above(2).getCenter();
+            ItemEntity diamond = new ItemEntity(level, spawnPos.x, spawnPos.y, spawnPos.z, new ItemStack(Items.DIAMOND));
+            diamond.setDeltaMovement(Vec3.ZERO);
+            level.addFreshEntity(diamond);
+        }
     }
 }
