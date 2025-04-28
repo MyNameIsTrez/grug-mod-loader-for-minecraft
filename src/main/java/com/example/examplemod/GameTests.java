99a6010dfead7c558e191de483a38d5392a31e16
MyNameIsTrez <welfje@gmail.com>
2025-04-28 20:46:30 +0200

    Add block_pos()

--- src/main/java/com/example/examplemod/GameTests.java
+++ src/main/java/com/example/examplemod/GameTests.java
@@ -8,7 +8,7 @@ import net.minecraftforge.gametest.GameTestHolder;
 
 @GameTestHolder(ExampleMod.MODID)
 public class GameTests {
-    @GameTest(template = ExampleMod.MODID+":foo")
+    @GameTest(template = ExampleMod.MODID+":placeholder_structure")
     public static void block(GameTestHelper helper) {
         Grug.resetVariables();
 
@@ -22,6 +22,21 @@ public class GameTests {
         helper.assertTrue(block != -1, "Invalid block");
 
         helper.assertTrue(Grug.fnEntities.contains(block), "fnEntities did not contain block");
+
+        helper.succeed();
+    }
+
+    @GameTest(template = ExampleMod.MODID+":placeholder_structure")
+    public static void block_pos(GameTestHelper helper) {
+        Grug.resetVariables();
+
+        Grug.globalEntities = new HashSet<>();
+        Grug.fnEntities = new HashSet<>();
+
+        long blockPos = GameFunctions.block_pos(0, 0, 0);
+        helper.assertTrue(blockPos != -1, "Invalid blockPos");
+
+        helper.assertTrue(Grug.fnEntities.contains(blockPos), "fnEntities did not contain blockPos");
 
         helper.succeed();
     }
