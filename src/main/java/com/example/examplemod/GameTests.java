35375db54f25e5325347549dfda45204e7241632
MyNameIsTrez <welfje@gmail.com>
2025-04-29 22:49:01 +0200

    Add test hash_set_add_to_global_set()

--- src/main/java/com/example/examplemod/GameTests.java
+++ src/main/java/com/example/examplemod/GameTests.java
@@ -1,5 +1,6 @@
 package com.example.examplemod;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 import net.minecraft.core.BlockPos;
@@ -20,12 +21,12 @@ public class GameTests {
         Grug.fnEntities = new HashSet<>();
 
         long resourceLocation = GameFunctions.resource_location("white_concrete");
-        helper.assertTrue(resourceLocation != -1, "Invalid resourceLocation");
+        helper.assertTrue(resourceLocation != -1, "Invalid resourceLocation " + resourceLocation);
 
         long block = GameFunctions.block(resourceLocation);
-        helper.assertTrue(block != -1, "Invalid block");
+        helper.assertTrue(block != -1, "Invalid block " + block);
 
-        helper.assertTrue(Grug.fnEntities.contains(block), "fnEntities did not contain block");
+        helper.assertTrue(Grug.fnEntities.contains(block), "fnEntities did not contain block " + block);
 
         helper.succeed();
     }
@@ -37,9 +38,9 @@ public class GameTests {
         Grug.fnEntities = new HashSet<>();
 
         long blockPos = GameFunctions.block_pos(0, 0, 0);
-        helper.assertTrue(blockPos != -1, "Invalid blockPos");
+        helper.assertTrue(blockPos != -1, "Invalid blockPos " + blockPos);
 
-        helper.assertTrue(Grug.fnEntities.contains(blockPos), "fnEntities did not contain blockPos");
+        helper.assertTrue(Grug.fnEntities.contains(blockPos), "fnEntities did not contain blockPos " + blockPos);
 
         helper.succeed();
     }
@@ -47,7 +48,7 @@ public class GameTests {
     @GameTest(template = ExampleMod.MODID+":placeholder")
     public static void block_flag_update_all(GameTestHelper helper) {
         int flag = GameFunctions.block_flag_update_all();
-        helper.assertTrue(flag == Block.UPDATE_ALL, "Did not return Block.UPDATE_ALL");
+        helper.assertTrue(flag == Block.UPDATE_ALL, "Did not return Block.UPDATE_ALL, but " + flag);
 
         helper.succeed();
     }
@@ -60,7 +61,7 @@ public class GameTests {
 
         long boxId = GameFunctions.box_i32(0);
 
-        helper.assertTrue(Grug.fnEntities.contains(boxId), "fnEntities did not contain boxId");
+        helper.assertTrue(Grug.fnEntities.contains(boxId), "fnEntities did not contain boxId " + boxId);
 
         helper.succeed();
     }
@@ -90,6 +91,66 @@ public class GameTests {
         GameFunctions.remove_block(blockPosId, levelId);
 
         helper.assertBlockNotPresent(block, relativeStructureBlockPos);
+
+        helper.succeed();
+    }
+
+    /*
+    This code is tested:
+    ```grug
+    hash_set: id = hash_set()
+
+    on_a() {
+        hash_set_add(hash_set, box_i32(1))
+    }
+
+    on_b() {
+        hash_set_add(hash_set, box_i32(2))
+
+        assert_hash_set_has_global_entities(hash_set, box_i32(1))
+        assert_hash_set_has_global_entities(hash_set, box_i32(2))
+    }
+    ```
+    */
+    @GameTest(template = ExampleMod.MODID+":placeholder")
+    public static void hash_set_add_to_global_set(GameTestHelper helper) {
+        Grug.resetVariables();
+
+        Grug.globalEntities = new HashSet<>();
+        Grug.fnEntities = Grug.globalEntities;
+
+        long hashSetId = GameFunctions.hash_set();
+
+        Grug.fnEntities = new HashSet<>();
+
+        long boxId1 = GameFunctions.box_i32(1);
+        GameFunctions.hash_set_add(hashSetId, boxId1);
+
+        Grug.fnEntities = new HashSet<>();
+
+        long boxId2 = GameFunctions.box_i32(2);
+        GameFunctions.hash_set_add(hashSetId, boxId2);
+
+        String hashSetString = GameFunctions.get_hash_set_string(hashSetId);
+        helper.assertTrue(hashSetString.equals("[17179869185, 17179869187]"), "Got unexpected hashSetString value '" + hashSetString + "'");
+
+        helper.assertTrue(Grug.globalEntities.size() == 3, "Grug.globalEntities.size() was expected to be 3, but was " + Grug.globalEntities.size());
+
+        helper.assertTrue(Grug.globalEntities.contains(hashSetId), "Grug.globalEntities did not contain hashSetId " + hashSetId);
+
+        HashMap<Object, Long> objects = Grug.getHashSetObjects(hashSetId);
+
+        Object boxId1Object = ExampleMod.grug.getObject(boxId1);
+        helper.assertTrue(boxId1Object != null, "boxId1Object was not supposed to be null");
+        Long realBoxId1 = objects.get(boxId1Object);
+        helper.assertTrue(realBoxId1 != null, "realBoxId1 was not supposed to be null");
+        helper.assertTrue(Grug.globalEntities.contains(realBoxId1), "Grug.globalEntities did not contain realBoxId1 " + realBoxId1);
+
+        Object boxId2Object = ExampleMod.grug.getObject(boxId2);
+        helper.assertTrue(boxId2Object != null, "boxId2Object was not supposed to be null");
+        Long realBoxId2 = objects.get(boxId2Object);
+        helper.assertTrue(realBoxId2 != null, "realBoxId2 was not supposed to be null");
+        helper.assertTrue(Grug.globalEntities.contains(realBoxId2), "Grug.globalEntities did not contain realBoxId2 " + realBoxId2);
 
         helper.succeed();
     }
