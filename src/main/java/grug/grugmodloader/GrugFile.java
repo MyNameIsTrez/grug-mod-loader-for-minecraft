96e0f82007d474f52251b7c79044624c7cecf730
MyNameIsTrez <welfje@gmail.com>
2025-05-25 22:55:28 +0200

    GrugFile does not need resourceMtimes anymore

--- src/main/java/grug/grugmodloader/GrugFile.java
+++ src/main/java/grug/grugmodloader/GrugFile.java
@@ -4,11 +4,13 @@ class GrugFile {
     public String name;
     public String entity;
     public String entityType;
+
     public long dll;
+
     public int globalsSize;
     public long initGlobalsFn;
+
     public long onFns;
-    public long resourceMtimes;
 
     public GrugFile() {
     }
