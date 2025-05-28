c3e3fa6a98d5d44194f5305c7b7ac45ead1507bb
MyNameIsTrez <welfje@gmail.com>
2025-05-29 01:21:40 +0200

    Refactor TestBlock.java, by moving complexity to GameTestHelper

--- src/main/java/grug/grugmodloader/gametests/GameTestsUtils.java
+++ src/main/java/grug/grugmodloader/gametests/GameTestsUtils.java
@@ -2,7 +2,9 @@ package grug.grugmodloader.gametests;
 
 import java.util.HashSet;
 
+import grug.grugmodloader.GameFunctions;
 import grug.grugmodloader.Grug;
+import net.minecraft.gametest.framework.GameTestHelper;
 
 public class GameTestsUtils {
     public static void reset() {
@@ -10,5 +12,35 @@ public class GameTestsUtils {
 
         Grug.globalEntities = new HashSet<>();
         Grug.fnEntities = new HashSet<>();
+    }
+
+    public static void assert_error_id(GameTestHelper h, long id) {
+        h.assertTrue(id == -1, "Expected the error id -1, but got " + id);
+    }
+
+    public static void assert_fn_entities_contains(GameTestHelper h, long id) {
+        h.assertTrue(Grug.fnEntities.contains(id), "Grug.fnEntities did not contain " + id);
+    }
+
+    public static void assert_game_function_error(GameTestHelper h, String expectedErrorString) {
+        h.assertTrue(Grug.gameFunctionError.equals(expectedErrorString), "Grug.gameFunctionError had the unexpected value '" + Grug.gameFunctionError + "'");
+    }
+
+    public static long block(GameTestHelper h, long resourceLocation) {
+        long block = GameFunctions.block(resourceLocation);
+        h.assertTrue(block != -1, "Invalid block " + block);
+        return block;
+    }
+
+    public static long box_i32(GameTestHelper h, int i32) {
+        long box = GameFunctions.box_i32(i32);
+        h.assertTrue(box != -1, "Invalid box " + box);
+        return box;
+    }
+
+    public static long resource_location(GameTestHelper h, String resourceLocationString) {
+        long resourceLocation = GameFunctions.resource_location(resourceLocationString);
+        h.assertTrue(resourceLocation != -1, "Invalid resourceLocation " + resourceLocation);
+        return resourceLocation;
     }
 }
--- src/main/java/grug/grugmodloader/gametests/TestBlock.java
+++ src/main/java/grug/grugmodloader/gametests/TestBlock.java
@@ -1,7 +1,6 @@
 package grug.grugmodloader.gametests;
 
 import grug.grugmodloader.GameFunctions;
-import grug.grugmodloader.Grug;
 import grug.grugmodloader.GrugModLoader;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
@@ -9,83 +8,44 @@ import net.minecraftforge.gametest.GameTestHolder;
 
 @GameTestHolder(GrugModLoader.MODID)
 public class TestBlock extends GameTestsUtils {
-    /*
-    ```grug
-    on_a() {
-        resource_location: id = resource_location("diamond_block")
-
-        block: id = block(resource_location)
-
-        assert_fn_entities_contains(block)
-    }
-    ```
-    */
     @GameTest(template = GrugModLoader.MODID+":placeholder")
-    public static void block(GameTestHelper helper) {
+    public static void block(GameTestHelper h) {
         reset();
 
-        long resourceLocation = GameFunctions.resource_location("diamond_block");
-        helper.assertTrue(resourceLocation != -1, "Invalid resourceLocation " + resourceLocation);
+        long resource_location = resource_location(h, "diamond_block");
 
-        long block = GameFunctions.block(resourceLocation);
-        helper.assertTrue(block != -1, "Invalid block " + block);
+        long block = block(h, resource_location);
 
-        helper.assertTrue(Grug.fnEntities.contains(block), "fnEntities did not contain " + block);
+        assert_fn_entities_contains(h, block);
 
-        helper.succeed();
+        h.succeed();
     }
 
-    /*
-    ```grug
-    on_a() {
-        box: id = box_i32(1)
-
-        block: id = block(box)
-        assert_error_id(block)
-
-        assert_game_function_error("block(): Expected resource_location, but got boxed_i32")
-    }
-    ```
-    */
     @GameTest(template = GrugModLoader.MODID+":placeholder")
-    public static void block_expected_resource_location(GameTestHelper helper) {
+    public static void block_expected_resource_location(GameTestHelper h) {
         reset();
 
-        long box = GameFunctions.box_i32(1);
-        helper.assertTrue(box != -1, "Invalid box " + box);
+        long box = box_i32(h, 1);
 
         long block = GameFunctions.block(box);
-        helper.assertTrue(block == -1, "Expected an invalid block, but got " + block);
+        assert_error_id(h, block);
 
-        helper.assertTrue(Grug.gameFunctionError.equals("block(): Expected resource_location, but got boxed_i32"), "gameFunctionError had the unexpected value '" + Grug.gameFunctionError + "'");
+        assert_game_function_error(h, "block(): Expected resource_location, but got boxed_i32");
 
-        helper.succeed();
+        h.succeed();
     }
 
-    /*
-    ```grug
-    on_a() {
-        resource_location: id = resource_location("foo")
-
-        block: id = block(resource_location)
-        assert_error_id(block)
-
-        assert_game_function_error("block(): Invalid resource_location")
-    }
-    ```
-    */
     @GameTest(template = GrugModLoader.MODID+":placeholder")
-    public static void block_error_invalid_resource_location(GameTestHelper helper) {
+    public static void block_error_invalid_resource_location(GameTestHelper h) {
         reset();
 
-        long resourceLocation = GameFunctions.resource_location("foo");
-        helper.assertTrue(resourceLocation != -1, "Invalid resourceLocation " + resourceLocation);
+        long resource_location = resource_location(h, "foo");
 
-        long block = GameFunctions.block(resourceLocation);
-        helper.assertTrue(block == -1, "Expected an invalid block, but got " + block);
+        long block = GameFunctions.block(resource_location);
+        assert_error_id(h, block);
 
-        helper.assertTrue(Grug.gameFunctionError.equals("block(): Invalid resource_location"), "gameFunctionError had the unexpected value '" + Grug.gameFunctionError + "'");
+        assert_game_function_error(h, "block(): Invalid resource_location");
 
-        helper.succeed();
+        h.succeed();
     }
 }
