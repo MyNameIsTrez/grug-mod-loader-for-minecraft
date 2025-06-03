05769bb2cb6af92ce45eec70568aea2d7fb88be4
MyNameIsTrez <welfje@gmail.com>
2025-06-03 20:51:38 +0200

    Fix iterator() not having 100% coverage

--- src/main/java/grug/grugmodloader/AssertEntityTypeException.java
+++ src/main/java/grug/grugmodloader/AssertEntityTypeException.java
@@ -4,4 +4,8 @@ public class AssertEntityTypeException extends Exception {
     public AssertEntityTypeException(GrugEntityType entityType, GrugEntityType expectedEntityType) {
         super("Expected " + StringUtils.getSnakeCase(expectedEntityType) + ", but got " + StringUtils.getSnakeCase(entityType));
     }
+
+    public AssertEntityTypeException(GrugEntityType entityType) {
+        super("Expected iterable, but got " + StringUtils.getSnakeCase(entityType));
+    }
 }
