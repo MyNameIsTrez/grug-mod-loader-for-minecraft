430e4c62c4d592fe318fb2278bed237e5dee650d
MyNameIsTrez <welfje@gmail.com>
2025-06-08 22:39:31 +0200

    Give GrugObject equals() and hashCode() methods

--- src/main/java/grug/grugmodloader/GrugObject.java
+++ src/main/java/grug/grugmodloader/GrugObject.java
@@ -1,11 +1,26 @@
 package grug.grugmodloader;
 
+import java.util.Objects;
+
 public class GrugObject {
-    GrugEntityType type;
-    Object object;
+    public final GrugEntityType type;
+    public final Object object;
 
     public GrugObject(GrugEntityType type, Object object) {
         this.type = type;
         this.object = object;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GrugObject other = (GrugObject)o;
+        return type == other.type && Objects.equals(object, other.object);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, object);
     }
 }
