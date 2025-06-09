40f1fd030e147fabb5ca5b49868706811d1d9806
MyNameIsTrez <welfje@gmail.com>
2025-06-09 18:32:11 +0200

    Test hash_map_remove_key()

---
+++ src/main/java/grug/grugmodloader/gametests/TestHashMapRemoveKey.java
@@ -0,0 +1,55 @@
+package grug.grugmodloader.gametests;
+
+import grug.grugmodloader.GrugModLoader;
+import net.minecraft.gametest.framework.GameTest;
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraftforge.gametest.GameTestHolder;
+
+@GameTestHolder(GrugModLoader.MODID)
+public class TestHashMapRemoveKey extends GameTestsUtils {
+    @GameTest(template = GrugModLoader.MODID+":placeholder")
+    public static void hash_map_remove_key(GameTestHelper h) {
+        reset(h);
+
+        long hash_map = hash_map();
+
+        hash_map_put(hash_map, box_i32(1), box_i32(42));
+        hash_map_put(hash_map, box_i32(2), box_i32(69));
+
+        hash_map_remove_key(hash_map, box_i32(1));
+
+        h.assertFalse(hash_map_has_key(hash_map, box_i32(1)), "hash_map contained key box_i32(1)");
+        h.assertTrue(hash_map_has_key(hash_map, box_i32(2)), "hash_map did not contain key box_i32(2)");
+
+        h.succeed();
+    }
+
+    @GameTest(template = GrugModLoader.MODID+":placeholder")
+    public static void hash_map_remove_key_hash_map_does_not_contain_key(GameTestHelper h) {
+        reset(h);
+
+        long hash_map = hash_map();
+
+        hash_map_put(hash_map, box_i32(1), box_i32(2));
+
+        hash_map_remove_key(hash_map, box_i32(2));
+        assert_no_error();
+
+        h.assertTrue(hash_map_has_key(hash_map, box_i32(1)), "hash_map did not contain box_i32(1)");
+
+        h.succeed();
+    }
+
+    @GameTest(template = GrugModLoader.MODID+":placeholder")
+    public static void hash_map_remove_key_expected_hash_map(GameTestHelper h) {
+        reset(h);
+
+        long box = box_i32(1);
+
+        hash_map_remove_key(box, box);
+
+        assert_game_function_error("hash_map_remove_key(): Expected hash_map, but got boxed_i32");
+
+        h.succeed();
+    }
+}
