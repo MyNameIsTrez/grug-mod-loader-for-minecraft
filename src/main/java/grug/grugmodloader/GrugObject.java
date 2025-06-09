cac399ae914ba28326d85638a091ab6aecdfd9ca
MyNameIsTrez <welfje@gmail.com>
2025-06-09 03:12:39 +0200

    Fix GrugObject its hashCode(), so it doesn't violate its contract anymore

--- src/main/java/grug/grugmodloader/GrugObject.java
+++ src/main/java/grug/grugmodloader/GrugObject.java
@@ -1,6 +1,9 @@
 package grug.grugmodloader;
 
+import java.util.IdentityHashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 public class GrugObject {
     public final GrugEntityType type;
@@ -16,21 +19,51 @@ public class GrugObject {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GrugObject other = (GrugObject)o;
-
-        // TODO: Not sure if this code is even correct
-        // Prevents infinite recursion when a GrugObject contains itself
-        // if (other.type == GrugEntityType.HashSet || other.type == GrugEntityType.HashMap) return false;
-
         return type == other.type && Objects.equals(object, other.object);
     }
 
     @Override
     public int hashCode() {
-        // TODO: Not sure if this code is even correct
-        // Prevents infinite recursion when a GrugObject contains itself
-        if (type == GrugEntityType.HashSet || type == GrugEntityType.HashMap) return type.hashCode();
+        return safeHashCode(this, new IdentityHashMap<>());
+    }
 
-        return Objects.hash(type, object);
+    private static int safeHashCode(Object obj, IdentityHashMap<Object, Boolean> seen) {
+        // Case 0: null values: return 0 hash
+        if (obj == null) return 0;
+
+        // Case 1: cycle detection: we've already seen this object, so return 0 to avoid infinite recursion
+        if (seen.containsKey(obj)) return 0;
+
+        // Mark this object as visited to detect future cycles
+        seen.put(obj, true);
+
+        // Case 2: if this is a GrugObject, recursively compute hash for its type and object fields
+        if (obj instanceof GrugObject go) {
+            return Objects.hash(go.type, safeHashCode(go.object, seen));
+        }
+
+        // Case 3: if this is a Set, combine hashCodes of elements
+        if (obj instanceof Set<?> set) {
+            int h = 0;
+            for (Object item : set) {
+                h += safeHashCode(item, seen);
+            }
+            return h;
+        }
+
+        // Case 4: if this is a Map, hash each entry by XORing the key and value hashes
+        if (obj instanceof Map<?, ?> map) {
+            int h = 0;
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                int keyHash = safeHashCode(entry.getKey(), seen);
+                int valueHash = safeHashCode(entry.getValue(), seen);
+                h += keyHash ^ valueHash;
+            }
+            return h;
+        }
+
+        // Case 5: default: non-collection type like Block or BoxedI32
+        return obj.hashCode();
     }
 
     @Override
